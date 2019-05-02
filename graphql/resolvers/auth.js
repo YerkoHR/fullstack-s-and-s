0f3e4a17e8d47438c8482ed374a98a478a5f3d60
Waimog <yerko.anton.roj@hotmail.com>
2019-05-02 14:25:38 -0400

    Login resolver added

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcript = require("bcryptjs");
 const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   createUser: args => {
@@ -23,5 +24,27 @@ module.exports = {
       .catch(err => {
         throw err;
       });
+  },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const isEqual = await bcript.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error("Invalid credentials");
+    }
+
+    const token = await jwt.sign(
+      { userId: user.id, email: user.email },
+      "supersecretkey",
+      { expiresIn: "1h" }
+    );
+    return {
+      userId: user.id,
+      token,
+      tokenExpiration: 1
+    };
   }
 };
